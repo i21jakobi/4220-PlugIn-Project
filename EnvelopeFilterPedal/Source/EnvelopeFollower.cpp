27b1a1dc398fc111dc292975eaf9cfc9e61b13ca
Jakobi <i21jakobi@icloud.com>
2025-03-18 19:35:41 -0500

    Added RMS and peak processing

--- EnvelopeFilterPedal/Source/EnvelopeFollower.cpp
+++ EnvelopeFilterPedal/Source/EnvelopeFollower.cpp
@@ -10,4 +10,32 @@
 
 #include "EnvelopeFollower.h"
 
+    
+    float calculateRMS (juce::AudioBuffer<float> & buffer, const int channel, const int numSamples){
+        
+        float sumSquares = 0.0f;
+        
+        for (int i = 0; i < numSamples; ++i){
+            sumSquares += buffer.getSample(channel, i) * buffer.getSample(channel, i);
+        }
+        
+        return std::sqrt(sumSquares/numSamples); // Compute RMS value
+        
+    }
+    
+    
+    float calculatePeak(juce::AudioBuffer<float> & buffer, const int channel, const int numSamples){
+        
+        float peak = 0.0f;
 
+        for (int i = 0; i < numSamples; ++i)
+                {
+        float absSample = std::abs(buffer.getSample(channel, i)); // Get absolute value
+        if (absSample > peak)
+            peak = absSample; // Track peak value
+    }
+
+    return peak; // Return the peak value in this block
+}
+ 
+    
